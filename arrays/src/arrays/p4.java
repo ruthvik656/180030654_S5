5cbc287dace3653d68d8f1009b8c7dd06cf44f8d
ruthv <ruthv@WINDOWS-171OHIB>
2021-05-10 21:59:34 +0530

    WTN_ARRAYS

---
+++ arrays/src/arrays/p4.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class p4 {  
+		 public static void main(String[] args) {
+		        int ascii[] = {97,98,99,100,101,102};
+		        for (int i = 0; i < ascii.length; i++) {
+		            System.out.print((char)ascii[i] + " ");
+		        }
+		        System.out.println();
+		    }
+		}
